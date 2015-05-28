cf31e73797d3d39eb49dffff37a9b6ff95aba219
gougoumemeda <a516550257@qq.com>
2015-05-28 23:46:39 +0800

    试图post数据到api，但是session还存在问题

--- server.py
+++ server.py
@@ -11,13 +11,42 @@
 #-------------------------------------------------------------------------------
 import requests
 import json
+import os
+import webbrowser
+
+crossyWebPrefix = "http://tq5124.dy.tongqu.me/crossy"
+
+def getScoreCookie(url):
+    result = requests.get(url)
+    sessionID = result.cookies.get("ASP.NET_SessionId", "none")
+    return sessionID
 
 def scoreQueryFunc():
     print "正在执行查询【成绩】..."
-    result = requests.get("http://electsys.sjtu.edu.cn/edu/login.aspx")
-    sessionID = result.cookies.get("ASP.NET_SessionId", "none")
-    if sessionID == "none":
+
+    sessionID = getScoreCookie("http://electsys.sjtu.edu.cn/edu/student/sdtMain.aspx")
+    print sessionID
+    postData = {'cookie':'ASP.NET_SessionId=' + sessionID}
+    result = requests.post(crossyWebPrefix + "/api/1/elect/info", params=postData)
+    print result.text
+
+    '''if result.status_code == 400:
         print "Session未保存，请重新登录"
+        os.system("C:\Program Files (x86)\Google\Chrome\Application\chrome.exe")
+        webbrowser.open("http://electsys.sjtu.edu.cn/edu/login.aspx")
+    print("完成登录时请输入\"login complete\"")
+
+    while (True):
+        a = raw_input()
+        if a == "login complete":
+            break
+
+    sessionID = getScoreCookie("http://electsys.sjtu.edu.cn/edu/student/sdtMain.aspx")
+    if sessionID != "none":
+        postData = {'cookie':'ASP.NET_SessionId=' + sessionID}
+        result = requests.post(crossyWebPrefix + "/api/1/elect/info", params=postData)
+        print result.text'''
+
 
 
 def booksQueryFunc():
