83c05d7b95cf2388190c86b4e0988b0a0933a5f2
gougoumemeda <a516550257@qq.com>
2015-05-29 22:35:33 +0800

    成绩读取与处理模块完成

--- server.py
+++ server.py
@@ -18,12 +18,26 @@ import win32crypt
 
 crossyWebPrefix = "http://tq5124.dy.tongqu.me/crossy"
 
-def getLocalCookie():
+#------------------------course information--------------------------
+class courses:
+    name = ""
+    credit = 0
+    score = 0
+    def __init__(self, n, c, s):
+        self.credit = 'credits: ' + c
+        self.name= 'name: ' + n
+        self.score = 'score: ' + s
+    def printCourse(self):
+        print self.name + ' - ' + self.credit + ' - ' + self.score
+#--------------------------------------------------------------------
+
+#------------------------------get cookies---------------------------
+def getLocalCookie(url, key):
     cookie_file_path = os.path.join(os.environ['LOCALAPPDATA'], r'Google\Chrome\User Data\Default\Cookies')
     conn = sqlite3.connect(cookie_file_path)
-    sql = 'select host_key, name, value, encrypted_value from cookies where host_key like "%{}%"'.format("electsys.sjtu.edu.cn")
+    sql = 'select host_key, name, value, encrypted_value from cookies where host_key like "%{}%"'.format(url)
     for i in conn.execute(sql):
-        if i[1] == "ASP.NET_SessionId":
+        if i[1] == key:
             pwdHash = str(i[3])
             try:
                 ret = win32crypt.CryptUnprotectData(pwdHash, None, None, None, 0)
@@ -35,36 +49,51 @@ def getLocalCookie():
     conn.close()
     file = open("cookie.txt", 'r')
     return file.readline()
+#--------------------------------------------------------------------
 
+#-------------------------deal with score query----------------------
+def dealWithScore(data):
+    scoreInfo = {}
+    for i in data:
+        crs = courses(i['name'], i['credit'], i['score'])
+        semester = i['year'] + '-' + i['semester']
+        if scoreInfo.has_key(semester):
+            scoreInfo[semester].append(crs)
+        else:
+            scoreInfo[semester] = [crs]
+    return scoreInfo
 
 def scoreQueryFunc():
     print "Query Score..."
 
-    sessionID = getLocalCookie()
+    sessionID = getLocalCookie("electsys.sjtu.edu.cn", "ASP.NET_SessionId")
     postData = {"cookie":"ASP.NET_SessionId=" + sessionID}
     result = requests.post(crossyWebPrefix + "/api/1/elect/info", postData)
 
     if result.status_code == 400:
-        print result.error
+        print "please login"
         os.system("C:\Program Files (x86)\Google\Chrome\Application\chrome.exe")
         webbrowser.open("http://electsys.sjtu.edu.cn/edu/login.aspx")
-
         print("Input \"login complete\" when you have logined")
         while (True):
             a = raw_input()
             if a == "login complete":
                 break
-        sessionID = getLocalCookie()
-        if sessionID != "none":
+        sessionID = getLocalCookie("electsys.sjtu.edu.cn", "ASP.NET_SessionId")
+        if sessionID != "":
+            print "Get Score Info, Waiting for minutes..."
             postData = {'cookie':'ASP.NET_SessionId=' + sessionID}
-            print postData
             result = requests.post(crossyWebPrefix + "/api/1/elect/info", postData)
 
-    print "Get Score Info, Waiting for minutes..."
-    print result.text
+    print "Deal With Score Info, Waiting for minutes..."
     scoreData = json.loads(result.text, encoding='utf-8')
-    print scoreData['info']['school']
+    scoreInfo = dealWithScore(scoreData['courses'])
+    for i in scoreInfo:
+        print i
+        for j in scoreInfo[i]:
+            j.printCourse()
 
+#---------------------------------------------------------------------
 
 def booksQueryFunc():
     print "Querying Books..."
